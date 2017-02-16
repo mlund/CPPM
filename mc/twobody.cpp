544d974cb29ec19ac9a6315fd641e95428c67565
Mikael Lund <mlund@mac.com>
2017-02-16 16:45:59 +0100

    Cuboid version of main program

--- mc/twobody.cpp
+++ mc/twobody.cpp
@@ -3,16 +3,18 @@
 using namespace Faunus;
 using namespace Faunus::Potential;
 
+#ifdef CUBOID
+typedef Space<Geometry::Cuboid> Tspace;
+#elif
 typedef Space<Geometry::Cylinder> Tspace;
-//typedef Space<Geometry::Cuboid> Tspace;
+#endif
 typedef CombinedPairPotential<WeeksChandlerAndersen, Coulomb> Tpairpot;
-//typedef CombinedPairPotential<HardSphere, Coulomb> Tpairpot;
 
 int main(int argc, char** argv) {
   InputMap mcp("twobody.json");
 
   Tspace spc(mcp);
-  spc.load("state"); // load previous state, if any
+  spc.load("state");
 
   auto pot = Energy::Nonbonded<Tspace,Tpairpot>(mcp) + Energy::MassCenterConstrain<Tspace>(mcp, spc);
 
@@ -22,18 +24,12 @@ int main(int argc, char** argv) {
   Analysis::CombinedAnalysis analysis(mcp, pot, spc);
   Move::Propagator<Tspace> mv(mcp,pot,spc);
 
-  //analysis.add( MeanForce<Tspace>(
-  //      { {"groups", {0,1} }, {"nstep", 10} },
-  //      pot, spc) );
-
   cout << atom.info() << spc.info() << pot.info() << textio::header("MC Simulation Begins!");
 
   MCLoop loop(mcp);
   while ( loop[0] ) {
     while ( loop[1] ) {
       mv.move();
-      //spc.groupList()[0]->setMassCenter(spc);
-      //spc.groupList()[1]->setMassCenter(spc);
       analysis.sample();
       rdf( spc.geo.dist( spc.groupList()[0]->cm, spc.groupList()[1]->cm ))++;
 
